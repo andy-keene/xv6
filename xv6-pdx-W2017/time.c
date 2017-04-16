5529e046d78dc4c2d02f828f9bfb7476bd7b9431
Andrew Keene <keene@babbage.cs.pdx.edu>
2017-04-15 22:59:58 -0700

    clean time cmd

--- xv6-pdx-W2017/time.c
+++ xv6-pdx-W2017/time.c
@@ -1,5 +1,6 @@
 #include "types.h"
 #include "user.h"
+//argv + x == &argv[x]
 
 void
 print_results(const char * cmd, const uint time)
@@ -17,11 +18,16 @@ main(int argc, char * argv[])
   int pid = fork();
 
   if(pid == 0){ //child
-    //argv + x == &argv[x]
-    exec(argv[1], &argv[1]);
+    int exec_ret = exec(argv[1], &argv[1]);
+
+    if(exec_ret < 0){
+      printf(2, "execution of %s failed\n", argv[0]);
+      exit();
+    }
   } else {
-    int ret = wait();
-    if(ret >= 0)
+    int wait_ret = wait();
+
+    if(wait_ret >= 0)
       print_results(argv[1], uptime() - start_time);
   }
 
